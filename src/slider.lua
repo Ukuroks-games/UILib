413f55a28ead539fff481e315e96347bacfcf1ca
Егор <148970641+Egor00f@users.noreply.github.com>
2025-03-31 12:46:13 +0500

    Create slider.lua

---
+++ src/slider.lua
@@ -0,0 +1,17 @@
+local UserInputService = game:GetService("UserInputService")
+
+local slider = {}
+
+export type Slider = {
+	
+}
+
+function silder.new(): Slider
+    local self = {
+		
+    }
+
+	return self
+end
+
+return slider
