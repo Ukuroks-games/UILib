69ac6a1919bc0fa26b9d3dd6ddb017d47cc62047
Егор <148970641+Egor00f@users.noreply.github.com>
2025-03-31 12:37:57 +0500

    Create window.lua

---
+++ src/window.lua
@@ -0,0 +1,20 @@
+local UserInputService = game:GetService("UserInputService")
+
+--[[
+    Window class
+]]
+local window = {}
+
+export type Window = {
+
+}
+
+function window.new(): Window
+    local self = {
+        
+    }
+
+    return self
+end
+
+return window
